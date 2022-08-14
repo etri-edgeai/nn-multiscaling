3a109473857aef721c8bc345e194e505cd02ded3
Jong-Ryul Lee <jongryul.lee@etri.re.kr>
2022-08-14 05:35:08 +0000

    fix scale
    Signed-off-by: Jong-Ryul Lee <jongryul.lee@etri.re.kr>

--- bespoke/base/interface.py
+++ bespoke/base/interface.py
@@ -132,7 +132,7 @@ class ModelHouse(object):
             print(len(nodes_))
             n = np.random.choice(self._nodes)
             tag = "app_origin" if n.tag == "origin" else "app_alter"
-            scale = np.random.choice([0.25, 0.375, 0.5, 0.625, 0.75, 0.875])
+            scale = np.random.choice([0.05, 0.075, 0.1, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75])
             sample_data = self._sample_inputs[n.pos[0]]
             alters = gen_.generate(n.net, [scale], sample_data=sample_data, custom_objects=self._custom_objects, init=init, pruning_exit=pruning_exit)
 
